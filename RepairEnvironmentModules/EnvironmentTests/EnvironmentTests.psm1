3d188d51659a23a4f3020cb178f827f34f9f517b
Tatham Oddie <tatham@oddie.com.au>
2012-10-09 14:59:03 +1100

    Add test for commands on path

---
+++ RepairEnvironmentModules/EnvironmentTests/EnvironmentTests.psm1
@@ -0,0 +1,18 @@
+﻿#requires -Version 2
+$ErrorActionPreference = "Stop"
+
+function Test-CommandAvailableInPath (
+	$CommandName
+) {
+	ExecuteTest `
+		-AssertionMessage "$CommandName is available in system path" `
+		-TestScript {
+			if ((Get-Command $CommandName -ErrorAction SilentlyContinue) -ne $null)
+			{
+				Write-Verbose "$CommandName is available in system path"
+				return $true
+			}
+			Write-TSProblem "$CommandName is not available in system path"
+			return $false
+		}
+}
